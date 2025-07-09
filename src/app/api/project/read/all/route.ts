006fbff35da2648c6621844fe24e005690e28a45
vinuth <vinuthsriarampath@outlook.com>
2025-07-09 08:36:22 +0530

    feature : implement /api/project/read/all api route to get all projects

---
+++ src/app/api/project/read/all/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try{
+        const projects = await prisma.project.findMany({
+            orderBy:{
+                updatedAt: "desc"
+            }
+        })
+        if(!projects || projects.length==0){
+            return NextResponse.json([],{status:204})
+        }
+        return NextResponse.json(projects,{status:200})
+    }catch(error){
+        console.log(error);
+        return NextResponse.json({error:"Internal server error!"},{status:500})
+    }
+}
