549b7200aa9be66c8ac869b86f4e09eda6a972a4
vinuth <vinuthsriarampath@outlook.com>
2025-07-16 07:22:32 +0530

    feature : implement User type interface

---
+++ src/types/User.ts
@@ -0,0 +1,13 @@
+export interface User{
+    id : number
+    first_name : String
+    last_name : String
+    contact : String
+    dob : Date
+    address : String
+    postal_code : number
+    email : String
+    bio? : String 
+    createdAt : Date
+    updatedAt : Date
+}
