478c6915c38f6c6b6e77d3885ffbde7e5e5230f7
vinuth <vinuthsriarampath@outlook.com>
2025-07-16 07:21:51 +0530

    feature : change seed to have the bio and the postal_code

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,6 +8,8 @@ async function main() {
   const last_name = process.env.USER_LAST_NAME || 'Last Name';
   const address = process.env.USER_ADDRESS || 'colombo';
   const contact = process.env.USER_CONTACT || '0000000000';
+  const postal_code = process.env.USER_POSTAL_CODE || parseInt('000000');
+  const bio = process.env.USER_BIO || "this is my bio";
   const dob = process.env.USER_DOB ?  new Date(process.env.USER_DOB) : new Date('2004-09-18');
 
   const user = await prisma.user.upsert({
@@ -21,6 +23,7 @@ async function main() {
       dob,
       email,
       password,
+      postal_code: parseInt(postal_code.toString())
     }
   });
 
